f299f292fb07f74db368bc16b19ea51f1c0bbbd5
phyzhangyang <yang.zhang@monash.edu>
2018-11-19 14:40:27 +1100

    更换取整函数

--- Internals/read_one_line.py
+++ Internals/read_one_line.py
@@ -5,7 +5,7 @@ import os
 
 if len(sys.argv) == 4 :
     if os.path.exists(sys.argv[2])==True: os.remove(sys.argv[2]) 
-    print int(float(open(sys.argv[3]).readline().rstrip()))
+    print round(float(open(sys.argv[3]).readline().rstrip()))
     try:
         open(sys.argv[2],'w').write(linecache.getline(sys.argv[1],int(float(open(sys.argv[3]).readline().rstrip()))))
     except:
