0fcc2c5446928c022ef187f0100b9e1b6f0f83ce
phyzhangyang <yang.phy@foxmail.com>
2023-11-02 22:53:46 +0800

    Modify the example to test the running time

--- tools/TestFunction.py
+++ tools/TestFunction.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # Test function
-# Used by the bin/example_*.ini
-# Yang, 2019.01.23
-from numpy import loadtxt, sin, cos
+# Used by the bin/example_*_.ini
+# Yang, 2023.11.01
+from numpy import loadtxt, sin, cos, random
+from time import sleep
 
 # read input from inp.dat
 data = loadtxt(r"TestFunction_input.dat")
 
 f = sin(data[0])**2 + cos(data[1])**2
+sleep(0.5+0.1*random.random())
 
 #open("TestFunction_output.dat",'w').write(str(f))
 open("TestFunction_output.dat",'w').write("test "+str(f))
